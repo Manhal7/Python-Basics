744cac0d3e20c054e0a56276f5cb96dcc445d96b
Manhal <mnhlaltmr6@gmail.com github>
2022-04-26 09:59:14 +0300

    add python conditions

---
+++ 01-conditions.py
@@ -0,0 +1,81 @@
+
+
+# simple condition
+
+x = 5
+
+if x == 3 :
+    print('x = 3')
+
+elif x = 5:
+    print('x = 5')
+
+else:
+    print(x)
+
+# indentations : spaces
+
+# Nested If :
+
+x = 6
+if x < 10 :
+    print('x < 10')
+
+    if x < 5:
+        print('x < 6')
+
+     else:
+         print('x > 6')
+
+else:
+    print('x > 10')
+
+
+
+
+
+## if with boolean operators:
+
+username = 'manhal'
+password = 12345
+
+
+if username == 'manhal' and password == 12345:
+    print('Welcome')
+
+if username == 'manhal7' or password == 12345678:
+    print('Welcome')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
