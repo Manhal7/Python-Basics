2d6b9e05d7c61f815679a58c266f4d91e0ec1eed
Manhal <mnhlaltmr6@gmail.com github>
2022-05-03 09:51:56 +0300

    Modules

---
+++ 08-modules.py
@@ -0,0 +1,26 @@
+# Module : Library
+
+# 1 : Internal 
+# 2 : External : user defined 
+
+
+# User Module :
+#         - Internal -----> import
+#         - External -----> install -----> import
+
+
+# install :
+#         - pip               -----> pip install django        # pipy
+#         - easy_install      -----> easy_install django       # github
+
+
+# import :
+
+#         import os ----> os.mkdir()                 # the best
+
+#         from os import mkdir , getcwd ----> mkdir()
+
+#         from os import*  ----> mkdir()
+# -----------------------------------------
+#         import Beautifullsoup4 as bs4              # as
+#         bs4.
